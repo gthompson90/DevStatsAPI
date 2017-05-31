c7a047f441996e44a21ad6943828a2b1d7fe4747
Mark Stott <mark.stott@cascadehr.co.uk>
2017-05-31 08:52:56 +0100

    Register Filters in the application

---
+++ DevStats/DevStats/App_Start/FilterConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using DevStats.Filters;
+
+namespace DevStats
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+            filters.Add(new IPAccessAttribute());
+        }
+    }
+}
--- DevStats/DevStats/Global.asax.cs
+++ DevStats/DevStats/Global.asax.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
-using System.Web.SessionState;
 using System.Web.Http;
 
 namespace DevStats
@@ -18,6 +14,7 @@ namespace DevStats
             UnityConfig.RegisterComponents();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
     }
 }
