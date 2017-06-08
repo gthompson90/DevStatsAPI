d824d83f78cf528cb30044207f8f2b80d4e05801
Mark Stott <mark.stott@cascadehr.co.uk>
2017-06-08 07:53:43 +0100

    Add a jira state checking api endpoint

---
+++ DevStats/DevStats/Controllers/API/JiraStateCheckerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using DevStats.Domain.Jira;
+using DevStats.Filters;
+
+namespace DevStats.Controllers.API
+{
+    [IPAccess]
+    public class JiraStateCheckerController : ApiController
+    {
+        private readonly IJiraService service;
+
+        public JiraStateCheckerController(IJiraService service)
+        {
+            if (service == null) throw new ArgumentNullException(nameof(service));
+
+            this.service = service;
+        }
+
+        [HttpGet]
+        [ResponseType(typeof(JiraStateSummary))]
+        public HttpResponseMessage Get([FromUri]string id)
+        {
+            try
+            {
+                var jiraItem = service.GetStateSummaries(id.ToUpper()).FirstOrDefault();
+
+                return Request.CreateResponse(HttpStatusCode.OK, jiraItem);
+            }
+            catch(Exception)
+            {
+                return Request.CreateResponse(HttpStatusCode.NoContent);
+            }
+        }
+    }
+}
