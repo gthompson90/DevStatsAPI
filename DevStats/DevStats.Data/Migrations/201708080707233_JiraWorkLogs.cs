1b83655cc675f3bea4ed9b5b8fab938831429331
Mark Stott <mark.stott@cascadehr.co.uk>
2017-08-08 08:08:06 +0100

    Add data migration for Jira work logs

---
+++ DevStats/DevStats.Data/Migrations/201708080707233_JiraWorkLogs.cs
@@ -0,0 +1,67 @@
+namespace DevStats.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class JiraWorkLogs : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.WorkLogEntry",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        WorkLogTaskID = c.Int(nullable: false),
+                        Worker = c.String(),
+                        EffortInSeconds = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.WorkLogTask", t => t.WorkLogTaskID, cascadeDelete: true)
+                .Index(t => t.WorkLogTaskID);
+            
+            CreateTable(
+                "dbo.WorkLogTask",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        WorkLogStoryID = c.Int(nullable: false),
+                        TaskKey = c.String(),
+                        Owner = c.String(),
+                        Activity = c.String(),
+                        Complexity = c.String(),
+                        EstimateInSeconds = c.Int(nullable: false),
+                        ActualTimeInSeconds = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.WorkLogStory", t => t.WorkLogStoryID, cascadeDelete: true)
+                .Index(t => t.WorkLogStoryID);
+            
+            CreateTable(
+                "dbo.WorkLogStory",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        StoryKey = c.String(),
+                        TShirtSize = c.String(),
+                        Complexity = c.String(),
+                        LooseEstimateInHours = c.Int(nullable: false),
+                        EstimateInSeconds = c.Int(nullable: false),
+                        ActualTimeInSeconds = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.WorkLogTask", "WorkLogStoryID", "dbo.WorkLogStory");
+            DropForeignKey("dbo.WorkLogEntry", "WorkLogTaskID", "dbo.WorkLogTask");
+            DropIndex("dbo.WorkLogTask", new[] { "WorkLogStoryID" });
+            DropIndex("dbo.WorkLogEntry", new[] { "WorkLogTaskID" });
+            DropTable("dbo.WorkLogStory");
+            DropTable("dbo.WorkLogTask");
+            DropTable("dbo.WorkLogEntry");
+        }
+    }
+}
