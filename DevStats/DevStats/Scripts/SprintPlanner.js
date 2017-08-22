3c32cfba94fb79188e9d47004398c188c517770f
Mark Stott <mark.stott@cascadehr.co.uk>
2017-08-22 13:24:48 +0100

    Allow moving of stories between the backlog and the sprint and calculate capacity remaining

--- DevStats/DevStats/Scripts/SprintPlanner.js
+++ DevStats/DevStats/Scripts/SprintPlanner.js
@@ -1,4 +1,7 @@
-﻿$(document).ready(function () {
+﻿var devCapacity = 0;
+var qaCapacity = 0;
+
+$(document).ready(function () {
     onTeamChange();
 
     $("#ddlSprints").change(function () {
@@ -70,14 +73,14 @@ function getSprintStories(boardid, sprintid) {
 }
 
 function fillSprintStories(data) {
-    $("div#sprint-content").html("<table id='tblsprintstories' class='defectdata'><thead><tr><th>Key</th><th>Description</th><th>Type</th><th>Refinement</th><th>State</th><th class='numeric'>Dev Remaining</th><th class='numeric'>QA Remaining</th></tr></thead><tbody></tbody></table>");
+    $("div#sprint-content").html(getHeaderRowMarkUp("tblsprintstories"));
 
     $.each(data, function (dataIndex) {
         var dataItem = data[dataIndex];
-        $("#tblsprintstories > tbody").append(getRowMarkUp(dataItem));
+        $("#tblsprintstories > tbody").append(getRowMarkUp(dataItem, true));
     });
 
-    addTotalRow("tblsprintstories");
+    addTotalRow("tblsprintstories", true);
 }
 
 function getRefinedStories(team, sprintid) {
@@ -89,14 +92,14 @@ function getRefinedStories(team, sprintid) {
 }
 
 function fillRefinedStories(data) {
-    $("div#refined-items").html("<table id='tblrefinedstories' class='defectdata'><thead><tr><th>Key</th><th>Description</th><th>Type</th><th>Refinement</th><th>State</th><th class='numeric'>Dev Remaining</th><th class='numeric'>QA Remaining</th></tr></thead><tbody></tbody></table>");
+    $("div#refined-items").html(getHeaderRowMarkUp("tblrefinedstories"));
 
     $.each(data, function (dataIndex) {
         var dataItem = data[dataIndex];
-        $("#tblrefinedstories > tbody").append(getRowMarkUp(dataItem));
+        $("#tblrefinedstories > tbody").append(getRowMarkUp(dataItem, false));
     });
 
-    addTotalRow("tblrefinedstories");
+    addTotalRow("tblrefinedstories", false);
 }
 
 function clearContainers() {
@@ -104,8 +107,25 @@ function clearContainers() {
     $("div#refined-items").empty();
 }
 
-function getRowMarkUp(story) {
-    var markUp = "<tr>";
+function getHeaderRowMarkUp(tableName) {
+    var markUp = "<table id='" + tableName + "' class='defectdata'>";
+    markUp += "<thead><tr>";
+    markUp += "<th>Key</th>";
+    markUp += "<th>Description</th>";
+    markUp += "<th>Type</th>";
+    markUp += "<th>Refinement</th>";
+    markUp += "<th>State</th>";
+    markUp += "<th class='numeric'>Dev Remaining</th>";
+    markUp += "<th class='numeric'>QA Remaining</th>";
+    markUp += "<th>Action</th>";
+    markUp += "</tr></thead>";
+    markUp += "<tbody></tbody></table>";
+
+    return markUp;
+}
+
+function getRowMarkUp(story, isInSprint) {
+    var markUp = "<tr id='" + story.Key+"'>";
     markUp += "<td><a href='" + story.Url + "' target='_blank'>" + story.Key + "</a></td>";
     markUp += "<td>" + story.Description + "</td>";
     markUp += "<td>" + story.Type + "</td>";
@@ -113,12 +133,20 @@ function getRowMarkUp(story) {
     markUp += "<td>" + story.State + "</td>";
     markUp += "<td class='numeric'>" + story.DevelopmentRemainingInHours + "</td>";
     markUp += "<td class='numeric'>" + story.TestingRemainingInHours + "</td>";
+
+    if (isInSprint) {
+        markUp += "<td><a onclick='javascript:removeFromSprint()' class='delete-button' data-key='" + story.Key + "'>[Remove]</a></td>";
+    }
+    else {
+        markUp += "<td><a onclick='javascript:addToSprint()' class='add-button' data-key='" + story.Key + "'>[Add]</a></td>";
+    }
+
     markUp += "</tr>";
 
     return markUp;
 }
 
-function addTotalRow(tableName) {
+function addTotalRow(tableName, includeCapacity) {
     var dataRows = $("#" + tableName).find("tbody>tr");
     var firstRow = dataRows[0];
     var totalDevRemaining = 0;
@@ -130,15 +158,99 @@ function addTotalRow(tableName) {
         totalQARemaining += parseFloat(dataRow.cells[6].innerText);
     });
 
-    var markUp = "<tfoot><tr>";
-    markUp += "<th>Total</th>";
+    var markUp = "<tfoot>"
+    markUp += "<tr>";
+    markUp += "<th colspan='5' class='numeric'>Total</th>";
+    markUp += "<th class='numeric' id='totalDevRemaining'>" + totalDevRemaining.toFixed(2) + "</th>";
+    markUp += "<th class='numeric' id='totalQaRemaining'>" + totalQARemaining.toFixed(2) + "</th>";
     markUp += "<th>&nbsp;</th>";
-    markUp += "<th>&nbsp;</th>";
-    markUp += "<th>&nbsp;</th>";
-    markUp += "<th>&nbsp;</th>";
-    markUp += "<th class='numeric'>" + totalDevRemaining.toFixed(2) + "</th>";
-    markUp += "<th class='numeric'>" + totalQARemaining.toFixed(2) + "</th>";
-    markUp += "</tr></tfoot>";
+    markUp += "</tr>";
 
+    if (includeCapacity) {
+        markUp += "<tr>";
+        markUp += "<th colspan='5' class='numeric'>Capacity</th>";
+        markUp += "<th><input type='number' id='devCapacity' min='0' max='500' value='" + devCapacity.toFixed(2) + "'></input></th>"
+        markUp += "<th><input type='number' id='qaCapacity' min='0' max='500' value='" + qaCapacity.toFixed(2) + "'></th>"
+        markUp += "<th>&nbsp;</th>";
+        markUp += "</tr>";
+
+        var devRemainingCapacity = devCapacity - totalDevRemaining;
+        var qaRemainingCapacity = qaCapacity - totalQARemaining;
+
+        markUp += "<tr>";
+        markUp += "<th colspan='5' class='numeric'>Remaining</th>";
+        markUp += "<th class='numeric' id='devRemainingCapacity'>" + devRemainingCapacity.toFixed(2) + "</th>";
+        markUp += "<th class='numeric' id='qaRemainingCapacity'>" + qaRemainingCapacity.toFixed(2) + "</th>";
+        markUp += "<th>&nbsp;</th>";
+        markUp += "</tr>";
+    }
+
+    markUp += "</tfoot>";
+
+    $("#" + tableName + " tfoot").remove();
     $("#" + tableName).append(markUp);
+
+    $("#devCapacity").blur(function () { onDevCapacityChange(); });
+    $("#qaCapacity").blur(function () { onQaCapacityChange(); });
+}
+
+function removeFromSprint() {
+    var key = $(event.srcElement).data("key");
+    var cells = $("tr#" + key).find("td");
+    var markUp = "<tr id='" + key + "'>";
+
+    $.each(cells, function (index) {
+        if (index < 7) {
+            markUp += cells[index].outerHTML;
+        }
+        else
+        {
+            markUp += "<td><a onclick='javascript:addToSprint()' class='add-button' data-key='" + key + "'>[Add]</a></td>"; 
+        }
+    });
+
+    markUp += "</tr>";
+
+    $("#tblrefinedstories > tbody").append(markUp);
+    $("#tblsprintstories tr#" + key).remove();
+    addTotalRow("tblsprintstories", true);
+    addTotalRow("tblrefinedstories", false);
+}
+
+function addToSprint() {
+    var key = $(event.srcElement).data("key");
+    var cells = $("tr#" + key).find("td");
+    var markUp = "<tr id='" + key + "'>";
+
+    $.each(cells, function (index) {
+        if (index < 7) {
+            markUp += cells[index].outerHTML;
+        }
+        else {
+            markUp += "<td><a onclick='javascript:removeFromSprint()' class='delete-button' data-key='" + key + "'>[Remove]</a></td>";
+        }
+    });
+
+    markUp += "</tr>";
+    
+    $("#tblsprintstories > tbody").append(markUp);
+    $("#tblrefinedstories tr#" + key).remove();
+    addTotalRow("tblsprintstories", true);
+    addTotalRow("tblrefinedstories", false);
+}
+
+function onDevCapacityChange() {
+    devCapacity = parseFloat($("#devCapacity").val());
+    var totalDevRemaining = parseFloat($("#totalDevRemaining").text());
+    var devCapacityRemaining = devCapacity - totalDevRemaining;
+
+    $("#devRemainingCapacity").text(devCapacityRemaining.toFixed(2));
+}
+
+function onQaCapacityChange() {
+    qaCapacity = parseFloat($("#qaCapacity").val());
+    var totalQaRemaining = parseFloat($("#totalQaRemaining").text());
+    var qaRemainingCapacity = qaCapacity - totalQaRemaining;
+
+    $("#qaRemainingCapacity").text(qaRemainingCapacity.toFixed(2));
 }
