f8033c99f3dc1ad1ab0a8db93e1890bea24b4390
Mark Stott <mark.stott@cascadehr.co.uk>
2017-09-12 09:01:01 +0100

    Add basic password validation

--- DevStats/DevStats/Models/Account/ResetPasswordModel.cs
+++ DevStats/DevStats/Models/Account/ResetPasswordModel.cs
@@ -5,14 +5,25 @@ namespace DevStats.Models.Account
     public class ResetPasswordModel
     {
         [Required]
+        [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
 
         [Required]
+        [Display(Name = "Reset Token")]
         public string Token { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "New Password")]
+        [MinLength(8)]
+        [MaxLength(50)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,50}$", ErrorMessage = "Invalid Password")]
         public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("NewPassword", ErrorMessage = "Passwords do not match")]
+        public string ComparePassword { get; set; }
     }
 }
