5d8b6022c8f2078523d3c8e22acba968186dc1aa
Mark Stott <mark.stott@cascadehr.co.uk>
2017-10-30 08:56:55 +0000

    Add update for Aha Defects to API posting tool

--- DevStats/DevStats/Models/Jira/ApiTestModel.cs
+++ DevStats/DevStats/Models/Jira/ApiTestModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 using DevStats.Domain.Jira;
 
@@ -17,7 +18,8 @@ namespace DevStats.Models.Jira
                 new SelectListItem { Value = "/api/JiraCreateSubtasks?issueId=@@id@@&displayIssueId=@@id@@", Text = "Story or Bug Created (Create Subtasks)" },
                 new SelectListItem { Value = "/api/JiraStoryUpdated?issueId=@@id@@&displayIssueId=@@id@@", Text = "Story or Bug Updated" },
                 new SelectListItem { Value = "/api/Jira/Delete/@@id@@", Text = "Story or Bug Deleted" },
-                new SelectListItem { Value = "/api/JiraSubtaskUpdated?issueId=@@id@@&displayIssueId=@@id@@", Text = "Subtask Updated" }
+                new SelectListItem { Value = "/api/JiraSubtaskUpdated?issueId=@@id@@&displayIssueId=@@id@@", Text = "Subtask Updated" },
+                new SelectListItem { Value = string.Format("/api/aha/GetDefectUpdates/{0:yyyy-MM-dd}", DateTime.Today.AddMonths(-2)), Text = "Get Defect Updates from Aha" }
             }; 
         }
     }
